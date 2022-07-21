dd06454ded837c1698e552908fa614c12fa1637d
Sherry <hhlsdkm@gmail.com>
2022-07-21 18:49:16 +0900

    li

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,10 +1,15 @@
 //import React from "react";
 
 import Todo from "../models/todo";
-import TodoList from "./TodoList";
 
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
-  return <TodoList items={props.items} />;
+  return (
+    <ul>
+      {props.items.map((item) => (
+        <li key={item.id}>{item.text}</li>
+      ))}
+    </ul>
+  );
 };
 
 export default Todos;
