d0fa6865d1bc78f91b2435084de91f9135b24251
Sherry90 <hhlsdkm@gmail.com>
2022-07-23 19:35:57 +0900

    dependency fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
+import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
 
 const App = () => {
-  const todos = [new Todo("Learn React"), new Todo("Learn Typesctipt")];
+    const todos = [new Todo("Learn React"), new Todo("Learn Typesctipt")];
 
-  return (
-    <div>
-      <Todos items={todos} />
-    </div>
-  );
+    return (
+        <div>
+            <NewTodo/>
+            <Todos items={todos}/>
+        </div>
+    );
 };
 
 export default App;
--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,25 +1,26 @@
-import { useRef } from "react";
-
+import {useRef} from "react";
 import React from "react";
 
 const NewTodo = () => {
     const todoTextInputRef = useRef<HTMLInputElement>(null);
 
-    const submitHandler = (event : React.FormEvent) => {
+    const submitHandler = (event: React.FormEvent) => {
         event.preventDefault();
+
+        const enteredText = todoTextInputRef.current!.value
+
+        if (enteredText.trim().length === 0) {
+            return
+        }
     }
 
-    const enteredText = todoTextInputRef.current!.value
-
-    if(enteredText.trim().length === 0){
-        return
-    }
-
-    return <form onSubmit={submitHandler}>
-        <label htmlFor={'text'}>Todo text</label>
-        <input type={'text'} id={'text'} ref={todoTextInputRef}/>
-        <button>Add Todo</button>
-    </form>
+    return (
+        <form onSubmit={submitHandler}>
+            <label htmlFor={'text'}>Todo text</label>
+            <input type={'text'} id={'text'} ref={todoTextInputRef}/>
+            <button>Add Todo</button>
+        </form>
+    );
 }
 
 export default NewTodo;
